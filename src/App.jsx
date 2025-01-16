8595e25e32c677b30e4ac361557d9b247250362b
seocylucky <seocylucky@github.com>
2025-01-16 23:14:07 +0900

    feat: react-helmet으로 검색 엔진 최적화

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/Home.jsx';
 import { Game } from './pages/Game/Game';
 import { Result } from './pages/Result/Result';
-import { GlobalStyle } from './components/GlobalStyle/GlobalStyle';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>띵~호와</title>
+        <meta name="description" content="재미있는 마우스 게임 띵호와" />
+        <meta name="keywords" content="게임, 철가방, 짜장면, 짬뽕, 띵호와" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
